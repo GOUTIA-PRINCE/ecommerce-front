053c1f5bb628494341be9fe804fc6685afee1cb4
GOUTIA-PRINCE <goutiaprince10@gmail.com>
2025-02-08 07:39:01 +0100

    Mon frontend a jours

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 
 @Component({
@@ -16,6 +18,10 @@ export class CheckoutComponent implements OnInit {
   
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
               private luv2ShopFormService: Luv2ShopFormService) { }
@@ -56,13 +62,22 @@ export class CheckoutComponent implements OnInit {
   }
 
   populateCreditCardDates() {
-    const startMonth: number = new Date().getMonth() + 1;
+    const startMonth: number = new Date().getMonth() + 1; // Correction de l'indexation du mois
     this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
       data => this.creditCardMonths = data
     );
 
+    // Populate credit card years  
     this.luv2ShopFormService.getCreditCardYears().subscribe(
       data => this.creditCardYears = data
+    );
+
+    // Populate countries
+    this.luv2ShopFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries: ", data);
+        this.countries = data;
+      }
     );
   }
 
@@ -71,8 +86,14 @@ export class CheckoutComponent implements OnInit {
     if (checkbox.checked) {
       this.checkoutFormGroup.controls['billingAddress']
             .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+          
+            //bug fix for states
+            this.billingAddressStates=this.shippingAddressStates;
     } else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+
+      //bug fix for states
+      this.billingAddressStates=[];
     }
   }
 
@@ -83,5 +104,52 @@ export class CheckoutComponent implements OnInit {
     }
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.value);
+    console.log("the email adress is"+this.checkoutFormGroup.get('customer')?.value.email);
+    console.log("the shipping adress country is"+this.checkoutFormGroup.get('shippingAdress')?.value.country.name);
+    console.log("the shipping adress state is"+this.checkoutFormGroup.get('shippingAdress')?.value.state.name);
+ 
+  }
+
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get("creditCard");
+    if (!creditCardFormGroup) return;
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+
+    let startMonth: number = (currentYear === selectedYear) ? new Date().getMonth() + 1 : 1;
+
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months:", data);
+        this.creditCardMonths = data;
+      }
+    );
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+    if (!formGroup) return;
+
+    const country = formGroup.value.country;
+    if (!country) return;
+
+    console.log(`${formGroupName} country code: ${country.code}`);
+    console.log(`${formGroupName} country name: ${country.name}`);
+
+    this.luv2ShopFormService.getStates(country.code).subscribe(
+      data => {
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data;
+        } else {
+          this.billingAddressStates = data;
+        }
+
+        // Select first item by default if available
+        if (data.length > 0) {
+          formGroup.get('state')?.setValue(data[0]);
+        }
+      }
+    );
   }
 }
